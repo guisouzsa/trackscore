25ffe78e6b0853ba9f91dfa14e1f19288da2459c
gui <168765125+guisouzsa@users.noreply.github.com>
2025-05-26 11:37:01 -0300

    login atualiado

---
+++ login_track/conexao.php
@@ -0,0 +1,16 @@
+<?php
+try {
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=trackscore;charset=utf8mb4",
+        "root",
+        "aluno",
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
+        ]
+    );
+    $pdo->exec("set names utf8mb4");
+} catch (PDOException $erro) {
+    echo "Erro na conexão: " . $erro->getMessage();
+}
+?>
