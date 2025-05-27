53f75d4ffa8aac7624f933b1e0c2055a7ad1abfb
guisouzsa <guilherme.souza103@aluno.ce.gov.br>
2025-05-26 22:41:46 -0300

    quase terminado

--- login.trackscore/login/painel.php
+++ login.trackscore/login/painel.php
@@ -1,15 +1,17 @@
 <?php
-require_once 'protege.php';
+include_once 'protect.php';
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>seja bem vindo</h1>
+    <h1>Bem vindo</h1>
+    <p>
+        <a href="logout.php">Sair</a>
+    </p>
 </body>
 </html>
