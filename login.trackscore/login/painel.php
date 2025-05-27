e072b55d79ae9962c0e0c1640a03ade0893f933e
gui <168765125+guisouzsa@users.noreply.github.com>
2025-05-26 22:56:23 -0300

    commit atualizado

---
+++ login.trackscore/login/painel.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'protect.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bem vindo</h1>
+    <p>
+        <a href="logout.php">Sair</a>
+    </p>
+</body>
+</html>
