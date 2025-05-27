46311d0a7ed017c1ac59eef84b35a9e902497154
gui <168765125+guisouzsa@users.noreply.github.com>
2025-05-27 14:57:25 -0300

    commit

---
+++ trackscore/login/logar.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require_once 'conexao.php';
+
+if(isset($_POST['email'], $_POST['senha'])) {
+    
+    $email = trim($_POST['email']);
+    $senha = trim($_POST['senha']);
+
+    if(empty($email)) {
+        echo "Preencha seu email";
+    } else if(empty($senha)) {
+        echo "Preencha sua senha";
+    } else {
+    
+        $sql = "SELECT * FROM usuarios WHERE email = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([':email' => $email]);
+
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($usuario && password_verify($senha, $usuario['senha'])) {
+            $_SESSION['id'] = $usuario['id'];
+            header("Location: painel.php");
+            exit;
+        } else {
+            echo "E-mail ou senha incorretos.";
+        }
+    }
+}
+?>
---
+++ trackscore/login/logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+session_destroy();
+header("location: index.php");
+?>
