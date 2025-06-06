26140e4b734f97ab2248b002f5fd88e4ab5a58e3
gui <168765125+guisouzsa@users.noreply.github.com>
2025-06-06 11:12:49 -0300

    atualização do navbar, contendo estilização de botoões.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://use.typekit.net/ysp4tri.css" />
+  <title>Trackscore</title>
+</head>
+<body>
+  <header>
+    <nav id="navbar_principal">
+
+      <div id="logo_img">
+        <img src="foto/logo.png" alt="Logo da Trackscore">
+      </div>
+
+      <form action="#" method="get">
+        <input type="text" placeholder="Pesquise por avaliações" name="pesquisar" id="pesquisa_box">
+      </form>
+
+      <button id="botao_avaliar">Crie sua avaliação</button>
+
+    </nav>
+  </header>
+
+  <main>
+    <section>
+
+      <div class="divs_principais">
+        <div class="div1">div 1</div>
+        <div class="div2">2</div>
+      </div>
+
+    </section>
+  </main>
+</body>
+</html>
