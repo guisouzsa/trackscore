ad66ca86625d96e576d004ba5a63fd340e65d1cf
gui <168765125+guisouzsa@users.noreply.github.com>
2025-06-17 09:06:27 -0300

    atualização dos boeôes e redirecionamento dos erros
    botão adicionado na tela de upload para quando clicar redirecionar para tela das tabelas, além disso, há atualização e estilização das mensagens de erro na tela de cadastro e login

--- cadastro/cadastrar_usuario.php
+++ cadastro/cadastrar_usuario.php
@@ -4,7 +4,6 @@ require_once '../login/conexao.php';
 if (!isset($_POST['nome'], $_POST['email'], $_POST['senha'])) {
     header("Location: cadastro.php?acessinvalid=1");
     exit;
-    // exit("Acesso inválido.");
 }
 
     $nome = trim($_POST['nome']);   
@@ -14,13 +13,13 @@ if (!isset($_POST['nome'], $_POST['email'], $_POST['senha'])) {
 if (!$nome || !$email || !$senha) {
         header("Location: cadastro.php?erro_campos=2");
     exit;
-    // exit("Por favor, preencha todos os campos.");
+
 }
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     header("Location: cadastro.php?email=3");
     exit;
-    // exit("E-mail inválido.");
+
 }
 
     $stmtCheck = $conexao->prepare("SELECT id FROM usuarios WHERE email = ?");
@@ -29,7 +28,7 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 if ($stmtCheck->rowCount()) {
         header("Location: cadastro.php?emailcadastrado=4");
     exit;
-    // exit("Este e-mail já está cadastrado.");
+
 }
 
 $stmt = $conexao->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)");
@@ -39,6 +38,6 @@ if ($stmt->execute([$nome, $email, password_hash($senha, PASSWORD_DEFAULT)])) {
 } else {
         header("Location: cadastro.php?erro_banco=5");
     exit;
-    // exit("Erro ao cadastrar usuário.");
+
 }
 ?>
